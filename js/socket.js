2a95990f693e349f4cdd117a0bdb72c0dd1403c2
tienpham <phamtien1709@gmail.com>
2017-12-20 15:37:06 +0700

    dsd

--- js/socket.js
+++ js/socket.js
@@ -1,21 +1,21 @@
 // import { setTimeout } from "timers";
 
-var socket = io("192.168.11.57:6969");
+// var socket = io("192.168.11.57:6969");
 
-socket.on("timeout_blue", (data) => {
-    if (data === "blue") {
-        Co.redWin = true;
-        setTimeout(function () {
-            Co.game.state.start('win');
-        }, 1200);
-    }
-});
+// socket.on("timeout_blue", (data) => {
+//     if (data === "blue") {
+//         Co.redWin = true;
+//         setTimeout(function () {
+//             Co.game.state.start('win');
+//         }, 1200);
+//     }
+// });
 
-socket.on("timeout_red", (data) => {
-    if (data === "red") {
-        Co.blueWin = true;
-        setTimeout(function () {
-            Co.game.state.start('win');
-        }, 1200);
-    }
-});
+// socket.on("timeout_red", (data) => {
+//     if (data === "red") {
+//         Co.blueWin = true;
+//         setTimeout(function () {
+//             Co.game.state.start('win');
+//         }, 1200);
+//     }
+// });
